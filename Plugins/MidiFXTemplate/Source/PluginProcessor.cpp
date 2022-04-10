8d9a2f9d3c55329cd9efc3a8c453fee961ee5b08
barmoshe <1barmoshe1@gmail.com>
2022-04-10 16:55:06 +0300

    smooth_plugin

--- Plugins/MidiFXTemplate/Source/PluginProcessor.cpp
+++ Plugins/MidiFXTemplate/Source/PluginProcessor.cpp
@@ -1,26 +1,111 @@
 #include "PluginProcessor.h"
+#include <vector>
+
+int compareArraysContents(const  int* array1,const  int* array2 )
+{
+    for (int i = 0; i < 3; ++i)
+    {
+        if(array1[i]!=array2[i])
+            return -1;
+    }
+    return 1;
+}
+int compareArraysContents2(const  int* array1,int note1,int note2,int note3 )
+{
+    if(note1==array1[0]&&note2==array1[1]&&note3==array1[2])
+    {
+        return 1;
+    }
+    return -1;
+}
 
 void MidiFXProcessor::processBlock(juce::AudioBuffer<float>& /*buffer*/,
                                    juce::MidiBuffer& midiMessages)
 
 {
+    note1->get();
+    note2->get();
+    note3->get();
+
     tempBuffer.clear();
+
+    if (auto ph = getPlayHead())
+    {
+        juce::AudioPlayHead::CurrentPositionInfo info;
+        ph->getCurrentPosition(info);
+    }
 
     for (auto m: midiMessages)
     {
         auto message = m.getMessage();
+
         auto samplePos = m.samplePosition;
+        if(message.isNoteOn())
+        {
+              noteRecived[0]=noteRecived[1];
+              noteRecived[1]=noteRecived[2];
+              noteRecived[2]=message.getNoteNumber();
+              if (compareArraysContents2(noteRecived, note1->get(),
+                                        note2->get(),
+                                        note3->get())==1)      {
+                  DBG("1");
+                  if(openFlag==0)
+                  {
+                      auto newMessage = juce::MidiMessage::controllerEvent(1, 1, 127);
+                      tempBuffer.addEvent(newMessage, samplePos);
 
-        //First, debug all incoming messages to console:
-        std::cout << "Incoming:" << message.getDescription()
-                  << " SamplePos:" << juce::String(samplePos) << std::endl;
+                      openFlag=1;
+                  }
+                  else
+                  {
+                    auto newMessage = juce::MidiMessage::controllerEvent(1, 1, 0);
+                      tempBuffer.addEvent(newMessage, samplePos);
+                      openFlag=0;
 
-        //Then, transpose all notes to be note #60
-        if (message.isNoteOnOrOff())
-            message.setNoteNumber(60);
+                  }
 
-        tempBuffer.addEvent(message, samplePos);
-    }
+              }else
+              if (compareArraysContents(noteRecived,noteToFind1)==1)      {
+                  DBG("2 N");
+                  if(openFlag1==0)
+                  {
+
+                      auto newMessage1 = juce::MidiMessage::controllerEvent(1, 2, 127);
+                      tempBuffer.addEvent(newMessage1, samplePos);
+                      openFlag1=1;
+                  }
+                  else
+                  {
+                      auto newMessage = juce::MidiMessage::controllerEvent(1,2, 0);
+                      tempBuffer.addEvent(newMessage, samplePos);
+                      openFlag1=0;
+
+                  }
+
+              }
+            }
+
+
+
+            //First, debug all incoming messages to console:
+            std::cout << "Incoming:" << message.getNoteNumber()
+                      << " SamplePos:" << juce::String(samplePos) << std::endl;
+
+            //Then, transpose all notes to be note #60
+            if (message.isNoteOn() && message.getNoteNumber() == 101)
+            {
+                auto newMessage = juce::MidiMessage::controllerEvent(1, 100, 127);
+                tempBuffer.addEvent(newMessage, samplePos);
+            }
+            else if (message.isNoteOff() && message.getNoteNumber() == 101)
+            {
+                //auto newMessage = juce::MidiMessage::controllerEvent(1, 100, 0);
+                // tempBuffer.addEvent(newMessage, samplePos);
+            }
+            else
+                tempBuffer.addEvent(message, samplePos);
+        }
+
 
     midiMessages.swapWith(tempBuffer);
 }
@@ -34,3 +119,5 @@ juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new MidiFXProcessor();
 }
+
+
--- Plugins/MidiFXTemplate/Source/PluginProcessor.h
+++ Plugins/MidiFXTemplate/Source/PluginProcessor.h
@@ -5,11 +5,33 @@
 class MidiFXProcessor : public PluginHelpers::ProcessorBase
 {
 public:
+    MidiFXProcessor()
+    {
+        note1 = new juce::AudioParameterInt("First", "First", 0, 127, 40);
+        note2 = new juce::AudioParameterInt("Second", "Second", 0, 127, 40);
+        note3 = new juce::AudioParameterInt("Third", "Third", 0, 127, 40);
+
+        addParameter(note1);
+        addParameter(note2);
+        addParameter(note3);
+    }
 
     void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
 
     juce::AudioProcessorEditor* createEditor() override;
 private:
 
+    juce::AudioParameterInt* note1;
+    juce::AudioParameterInt* note2;
+    juce::AudioParameterInt* note3;
+
+    std::vector<int> pressedNotes;
     juce::MidiBuffer tempBuffer;
+    int noteToFind1[3] {50,51,52};
+    int noteRecived[3] {-1,-1,-1};
+    int openFlag=0;
+    int openFlag1=0;
+
+
 };
